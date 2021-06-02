5e809de7f896845724cb25ef82b396a0894af9b9
unknown <faizanmdkhan99@gmail.com>
2021-06-02 15:11:39 +0530

    Commited file

---
+++ calculate.cs
@@ -0,0 +1,18 @@
+using System;
+namespace calculateapp
+{
+    public class calculate{
+        public void add(int a, int b){
+            Console.WriteLine(a+b);
+        }
+        public void subtract(int a, int b){
+            Console.WriteLine(a-b);
+        }
+        public void product(int a, int b){
+            Console.WriteLine(a*b);
+        }
+        public void divide(int a, int b){
+            Console.WriteLine(a/b);
+        }
+    }
+}
